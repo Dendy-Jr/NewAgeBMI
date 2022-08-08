65703283cea1a00ede49c5f16a2b2425959f7b78
Dendi188 <76904012+Dendi188@users.noreply.github.com>
2022-08-08 17:18:07 +0300

    Updated MainActivity design

--- app/src/main/java/com/olehvynnytskyi/android/bmi/app/MainActivity.kt
+++ app/src/main/java/com/olehvynnytskyi/android/bmi/app/MainActivity.kt
@@ -1,11 +1,15 @@
-package com.olehvynnytskyi.android.bmi
+package com.olehvynnytskyi.android.bmi.app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.olehvynnytskyi.android.bmi.R
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //TODO Create logic to display splash screen
     }
 }
